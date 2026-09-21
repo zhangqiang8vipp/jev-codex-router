bda09d5d937c49a75dbde5bcd06caffacaa0ff4f
zhangqiang8vipp <zhangqiang8vipp@163.com>
2026-09-21 11:30:18 +0800

    fix: release Auto toggle mutex only when owned

--- desktop/JevAutoToggle/App.xaml.cs
+++ desktop/JevAutoToggle/App.xaml.cs
@@ -6,11 +6,13 @@ namespace JevCodexAutoToggle;
 public partial class App : Application
 {
     private Mutex? _singleInstance;
+    private bool _ownsMutex;
     private OverlayController? _controller;
 
     private void App_OnStartup(object sender, StartupEventArgs e)
     {
         _singleInstance = new Mutex(true, @"Local\JevCodexAutoToggle", out var createdNew);
+        _ownsMutex = createdNew;
         if (!createdNew)
         {
             Shutdown();
@@ -24,7 +26,10 @@ public partial class App : Application
     protected override void OnExit(ExitEventArgs e)
     {
         _controller?.Dispose();
-        _singleInstance?.ReleaseMutex();
+        if (_ownsMutex)
+        {
+            try { _singleInstance?.ReleaseMutex(); } catch (ApplicationException) { }
+        }
         _singleInstance?.Dispose();
         base.OnExit(e);
     }
